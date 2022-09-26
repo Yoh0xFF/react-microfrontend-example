923852efe9964ddf821732fc57592b9e3a0b089e
Ioram Gordadze <ioramgordadze@fb.com>
2022-09-26 17:09:21 +0100

    Add prod configurations.

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -10,13 +10,14 @@ const prodConfig = {
 
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
 
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
