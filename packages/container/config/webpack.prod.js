36dd5eb5039ee848293e41ea08a0cb73b41f5a6c
Ioram Gordadze <ioramgordadze@fb.com>
2022-10-03 20:53:40 +0100

    Tiny fix.

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -18,7 +18,7 @@ const prodConfig = {
       name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@http://${domain}/auth/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
         dashboard: `dashboard@${domain}/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
