837b5aae1c209b544e62dd02408039601ffa441f
Ioram Gordadze <ioramgordadze@fb.com>
2022-10-03 20:47:19 +0100

    Tiny fix.

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -18,7 +18,6 @@ export default ({ history, onSignIn }) => {
             <Route path='/auth/signin'>
               <Signin onSignIn={onSignIn} />
             </Route>
-
             <Route path='/auth/signup'>
               <Signup onSignIn={onSignIn} />
             </Route>
